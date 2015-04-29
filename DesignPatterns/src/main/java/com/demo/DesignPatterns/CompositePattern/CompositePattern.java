03f1234c75f75c3b49dfb5b32d07c836fde62417
zeq9069 <906928204@qq.com>
2015-04-29 13:30:36 +0800

    update the CompositePattern

--- DesignPatterns/src/main/java/com/demo/DesignPatterns/CompositePattern/CompositePattern.java
+++ DesignPatterns/src/main/java/com/demo/DesignPatterns/CompositePattern/CompositePattern.java
@@ -3,6 +3,10 @@ package com.demo.DesignPatterns.CompositePattern;
  * ***********************
  * 
  *  组合模式
+ *  
+ *	组合模式有时又叫部分-整体模式在处理类似树形结构的问题时比较方便
+ *
+ *	使用场景：将多个对象组合在一起进行操作，常用于表示树形结构中，例如二叉树，数等。
  *
  * ***********************
  * @author kyrin kyrincloud@qq.com 
