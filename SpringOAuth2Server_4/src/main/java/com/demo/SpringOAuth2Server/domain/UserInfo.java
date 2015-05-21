90519202a452cc4f03b969d6fb7e1def326bd601
zeq9069 <906928204@qq.com>
2015-05-21 20:30:53 +0800

    add the support of clientInfo schema

---
+++ SpringOAuth2Server_4/src/main/java/com/demo/SpringOAuth2Server/domain/UserInfo.java
@@ -0,0 +1,110 @@
+package com.demo.SpringOAuth2Server.domain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserInfo implements UserDetails{
+
+	private static final long serialVersionUID = -6476235202408577967L;
+
+	private int id;
+	private String name;
+	private String password;
+	private int status;
+	private String desc;
+	private List<Role> roles;
+	
+	@SuppressWarnings("serial")
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> list=new ArrayList<GrantedAuthority>();
+		for(Role role:roles){
+			list.add(new GrantedAuthority() {
+				@Override
+				public String getAuthority() {
+					return role.getName();
+				}
+			});
+		}
+		return list;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.name;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return status==1?true:false;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+	
+}
---
+++ SpringOAuth2Server_4/src/main/java/com/demo/SpringOAuth2Server/service/UserInfoService.java
@@ -0,0 +1,7 @@
+package com.demo.SpringOAuth2Server.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserInfoService extends UserDetailsService{
+
+}
