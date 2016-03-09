76334198d5210a02f65d7d556577efd3c78f3c2b
zeq9069 <906928204@qq.com>
2016-03-09 18:02:38 +0800

    add spring session

--- OltuDemo/src/main/java/com/kyrin/OltuDemo/App.java
+++ OltuDemo/src/main/java/com/kyrin/OltuDemo/App.java
@@ -2,8 +2,11 @@ package com.kyrin.OltuDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class App {
     public static void main( String[] args ){
     	SpringApplication.run(App.class);
