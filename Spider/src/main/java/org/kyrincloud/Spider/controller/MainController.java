2c7b0461c33d9a0e00217a29908c15d541f87e84
zeq9069 <906928204@qq.com>
2016-03-14 16:39:58 +0800

    change to web

---
+++ Spider/src/main/java/org/kyrincloud/Spider/controller/MainController.java
@@ -0,0 +1,14 @@
+package org.kyrincloud.Spider.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value="/")
+	public void name() {
+		
+	}
+
+}
