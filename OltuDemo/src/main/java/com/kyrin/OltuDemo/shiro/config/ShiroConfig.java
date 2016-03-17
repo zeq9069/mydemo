67505118ce50e77cd98b15de3d353bdb7e44b820
zeq9069 <906928204@qq.com>
2016-03-17 13:44:37 +0800

    update

--- OltuDemo/src/main/java/com/kyrin/OltuDemo/shiro/config/ShiroConfig.java
+++ OltuDemo/src/main/java/com/kyrin/OltuDemo/shiro/config/ShiroConfig.java
@@ -7,7 +7,6 @@ import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
