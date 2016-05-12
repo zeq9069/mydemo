a7d392b09524b327baf7e874adefc6fe9ce35341
zeq9069 <906928204@qq.com>
2016-05-12 15:04:56 +0800

    update CRUD

--- MybatisDemo/src/main/java/com/kyrin/MybatisDemo/Bootstrap.java
+++ MybatisDemo/src/main/java/com/kyrin/MybatisDemo/Bootstrap.java
@@ -13,14 +13,74 @@ import com.kyrin.MybatisDemo.domain.User;
  *
  */
 public class Bootstrap {
-    public static void main( String[] args ){
-    	String config="config.xml";
-    	InputStream is=Thread.currentThread().getContextClassLoader().getResourceAsStream(config);
-    	SqlSessionFactoryBuilder build=new SqlSessionFactoryBuilder();
-    	SqlSessionFactory ssf=build.build(is);
-    	SqlSession session=ssf.openSession();
-    	String statement="com.kyrin.MybatisDemo.mapping.userMapper.getUser";
-    	User user=session.selectOne(statement,1);
-    	System.out.println(user);
-    }
+
+	static InputStream is;
+	static SqlSessionFactoryBuilder build;
+	static SqlSessionFactory ssf;
+	static String config = "config.xml";
+
+	static {
+		is = Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream(config);
+		build = new SqlSessionFactoryBuilder();
+		ssf = build.build(is, "development");
+	}
+
+	public static User selectOne() {
+		SqlSession session = ssf.openSession();
+		String statement = "com.kyrin.MybatisDemo.mapping.userMapper.getUser";
+		try {
+			return session.selectOne(statement, 1);
+		} finally {
+			session.close();
+		}
+	}
+	
+	public static int addUser(User user){
+		SqlSession session = ssf.openSession();
+		String statement = "com.kyrin.MybatisDemo.mapping.userMapper.addUser";
+		try {
+			int number=session.insert(statement,user);
+			session.commit();
+			return number;
+		} finally {
+			session.close();
+		}
+	}
+	
+	public static int deleteUser(int id){
+		SqlSession session = ssf.openSession();
+		String statement = "com.kyrin.MybatisDemo.mapping.userMapper.deleteUser";
+		try {
+			int number=session.delete(statement,id);
+			session.commit();
+			return number;
+		} finally {
+			session.close();
+		}
+	}
+	
+	public static int updateUser(User user){
+		SqlSession session = ssf.openSession();
+		String statement = "com.kyrin.MybatisDemo.mapping.userMapper.updateUser";
+		try {
+			int number=session.update(statement,user);
+			session.commit();
+			return number;
+		} finally {
+			session.close();
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(selectOne());
+		System.out.println("The number of rows affected by the insert : "+addUser(new User("Jack", "jack@gmail.com", "123")));
+		System.out.println("The number of rows affected by the delete : "+deleteUser(13));
+		User user=new User("Jack", "jack@qq.com", "123");
+		user.setId(1);
+		System.out.println("The number of rows affected by the update : "+updateUser(user));
+		
+		
+	}
 }
--- MybatisDemo/src/main/java/com/kyrin/MybatisDemo/domain/User.java
+++ MybatisDemo/src/main/java/com/kyrin/MybatisDemo/domain/User.java
@@ -14,6 +14,15 @@ public class User {
 	private String email;
 	
 	private String password;
+	
+	public User() {
+	}
+	
+	public User(String username,String email,String password){
+		this.username=username;
+		this.email=email;
+		this.password=password;
+	}
 
 	public int getId() {
 		return id;
