fedd8ecb95d20a56a8224a23a8a907484b3662eb
erqiang.zhang <erqiang.zhang@baichanghui.com>
2017-02-05 13:08:55 +0800

    JMI demo

---
+++ JMIDemo/src/main/java/com/kyrincloud/JMIDemo/App.java
@@ -0,0 +1,13 @@
+package com.kyrincloud.JMIDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
